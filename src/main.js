0b8b543baaec3888a53e751d4a462b9cbe42a55e
Leo Diaz <leoangelo.diaz@gmail.com>
2018-02-25 02:26:11 +0800

    add dependecies

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from '@/app'
+import router from '@/router'
+import Vuetify from 'vuetify'
+import 'babel-polyfill'
 
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
