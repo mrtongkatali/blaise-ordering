f0efa09d8cd96891844c36da740763ec36223201
Leo Diaz <leoangelo.diaz@gmail.com>
2018-03-01 23:26:52 +0800

    cleanup

--- src/pages/web/products.vue
+++ src/pages/web/products.vue
@@ -1,82 +1,28 @@
 <template lang="pug">
-v-layout(row, wrap)
-  v-flex(xs12)
-    v-text-field(append-icon="search", label="Search", single-line, hide-details, v-model="search").mb-2
+  v-layout(row, wrap)
+    v-flex(xs12)
+      v-text-field(append-icon="search", label="Search", single-line, hide-details, v-model="search").mb-2
 
-    v-data-table.elevation-1(:headers="headers", :items="productList", :search="search", :rows-per-page-items="rowPerPageItems")
-      template(slot="headerCell", slot-scope="props")
-        v-tooltip(bottom)
-          span(slot="activator")
-            | {{ props.header.text }}
-          span
-            | {{ props.header.text }}
-      template(slot="items", slot-scope="props")
-        tr(@click.stop="selectSupplier(props.item)").pointer
-          td {{ props.item.id }}
-          td {{ props.item.name }}
-          td {{ props.item.address }}
-          td {{ props.item.currency }}
-          td.text-xs-left {{ props.item.status }}
-          td.text-xs-left {{ dateFormat(props.item.created_at) }}
-          td.text-xs-left
-            v-btn( light, small, color="red", @click.native.stop="deleteSupplier(props.item)").white--text Delete
+      v-data-table.elevation-1(:headers="headers", :items="productList", :search="search", :rows-per-page-items="rowPerPageItems")
+        template(slot="headerCell", slot-scope="props")
+          v-tooltip(bottom)
+            span(slot="activator")
+              | {{ props.header.text }}
+            span
+              | {{ props.header.text }}
+        template(slot="items", slot-scope="props")
+          tr(@click.stop="selectSupplier(props.item)").pointer
+            td {{ props.item.id }}
+            td {{ props.item.name }}
+            td {{ props.item.address }}
+            td {{ props.item.currency }}
+            td.text-xs-left {{ props.item.status }}
+            td.text-xs-left {{ dateFormat(props.item.created_at) }}
+            td.text-xs-left
+              v-btn( light, small, color="red", @click.native.stop="deleteSupplier(props.item)").white--text Delete
 
-  v-dialog(v-model="products.show", max-width="500px", persistent)
-    v-form(ref="createProductForm")
-      v-card
-        v-card-title.title {{ (products.selected ? 'Update Product' : 'Add Product') }}
-        v-card-text
-          v-layout(row, wrap)
-            v-flex(xs12)
-              v-text-field(
-                light,
-                single-line,
-                prepend-icon="mdi-shape",
-                v-model="input.name",
-                label="Product Name",
-                :rules="[inputRules()['required']]"
-                required
-              )
-            v-flex(xs12)
-              v-text-field(
-                light,
-                required,
-                single-line,
-                prepend-icon="mdi-map-marker",
-                v-model="input.address",
-                label="Address",
-                :rules="[inputRules()['required']]"
-              )
-            v-flex(xs12)
-              v-text-field(
-                light,
-                required,
-                single-line,
-                prepend-icon="mdi-currency-usd",
-                v-model="input.currency",
-                label="Currency",
-                :rules="[inputRules()['required']]"
-              )
-        v-card-actions.display-flex.justify-end
-          v-btn(color="red", flat, @click.stop="cancelModal", :disabled="loading.create") Close
-          v-btn(
-            v-if="!products.selected"
-            medium,
-            color="primary",
-            :loading="loading.create",
-            @click.stop="createSupplier"
-          ) Add Product
-
-          v-btn(
-            v-if="products.selected"
-            medium,
-            color="primary",
-            :loading="loading.create",
-            @click.stop="updateSupplier(products.selected)"
-          ) Update Product
-
-  v-btn(fab, bottom, right, color="green", dark, fixed, @click.stop="$router.push({name: 'product-create'})")
-    v-icon add
+    v-btn(fab, bottom, right, color="green", dark, fixed, @click.stop="$router.push({name: 'product-create'})")
+      v-icon add
 </template>
 
 <script>
@@ -116,72 +62,8 @@ export default {
   },
 
   methods: {
-    async updateSupplier (supplierId) {
-      this.loading.create = false
-      if (!this.$refs.createProductForm.validate()) return
-
-      try {
-        this.loading.create = true
-        await ProductService.updateSupplier({
-          supplierId: supplierId,
-          name: this.input.name,
-          address: this.input.address,
-          currency: this.input.currency
-        })
-
-        this.displayToast(`Supplier has been updated`)
-        this.loadSupplierList()
-        this.cancelModal()
-        this.$refs.createProductForm.reset()
-        this.loading.create = false
-      } catch (e) {
-        this.loading.create = false
-        this.displayToast(this.parseErrorRes(e) || 'An error occured when trying to update supplier. Please try again', 'error')
-      }
-    },
-
-    cancelModal () {
-      this.products.show = false
-      this.input = {
-        name: null,
-        address: null,
-        currency: null
-      }
-
-      this.products.selected = null
-    },
-
     dateFormat (datetime) {
       return (datetime ? moment(datetime).format(this.timeformat()) : '')
-    },
-
-    async deleteSupplier (supplier) {
-      try {
-        await ProductService.updateSupplier({
-          supplierId: supplier.id,
-          name: supplier.name,
-          address: supplier.address,
-          currency: supplier.currency,
-          status: 'inactive'
-        })
-
-        this.displayToast(`Successfully deleted Supplier - ${supplier.name}`)
-        this.loadSupplierList()
-      } catch (e) {
-        console.log(`e`, e)
-        this.displayToast(this.parseErrorRes(e) || 'An error occured when trying to delete supplier. Please try again', 'error')
-      }
-    },
-
-    selectSupplier (supplier) {
-      this.input = {
-        name: supplier.name,
-        address: supplier.address,
-        currency: supplier.currency
-      }
-
-      this.products.selected = supplier.id
-      this.products.show = true
     },
 
     async createSupplier () {
